dc44ef87fc71c2a81957858b887549dd34b6df05
김현희 <hyunhee@nuritelecom.com>
2020-05-15 16:31:43 +0900

    Initial commit

---
+++ src/main/java/com/aimir/scheduler/SpringSchedulerApp.java
@@ -0,0 +1,12 @@
+package com.aimir.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulerApp {
+    public static void main(String[] args) {
+        System.out.println("SpringSchedulerApp START!!");
+        SpringApplication.run(SpringSchedulerApp.class);
+    }
+}
